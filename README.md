5f31ce69e1608b13681653a8efa03b1e0f28ed11
noman-noor <noman.noor@hotmail.com>
2024-04-15 18:33:41 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Repocket Android SDK Demo
+
+
+## Getting Started
+
+### Step 1: Configure dependency URL
+#### Add snippet below to settings.gradle file:
+
+```groovy
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+    maven {
+      url = uri("https://jitpack.io")
+      credentials {
+        username = "YOUR-AUTH-TOKEN"
+      }
+    }
+  }
+}
+```
+
+### Step 2: Embed dependencies
+
+Add dependency in you Android project app level  `build.gradle`. 
+
+```groovy
+dependencies {
+  implementation 'org.bitbucket.consumerchoicemvp:repocket-android-sdk:LATEST-VERSION'
+}
+```
+
+### Step 3 initialize SDK
+
+Initialize SDK on you `Application main class`
+
+```groovy
+val repocket= Repocket.Builder().withContext(this).withApiKey("YOUR-API-KEY").build()
+```
+
+### Step 4 Connect SDK
+You can connect SDK by just calling `connnect` function
+
+```groovy
+repocket.connect()
+```
+
+### Step 5 (Optional)
+
+You can listen connection related events by collecting `connection flow`
+
+```groovy
+lifecycleScope.launch {
+  repocket.connectionStatus.collect {
+    when (it) {
+      ConnectionEvent.Connected -> {
+        // connected state
+      }
+
+      ConnectionEvent.Connecting -> {
+        // connecting state
+      }
+
+      ConnectionEvent.Disconnected -> {
+        // disconnected state
+
+      }
+
+      is ConnectionEvent.Error -> {
+        // Error state
+      }
+
+      ConnectionEvent.OnRefreshTokenRequired -> {
+        // Refresh Token required state
+      }
+    }
+
+  }
+}
+
+```
+
+You can disconnect SDK by calling `disconnect` function
+
+```groovy
+repocket.disconnect()
+```
+
+
---
+++ app/src/main/java/com/geonode/testsdk/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.geonode.testsdk
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.geonode.testsdk.ui.theme.TestSdkTheme
+import com.repocket.androidsdk.Repocket
+import com.repocket.androidsdk.models.ConnectionEvent
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    private val TAG = "SDK-TEST"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        val textView = findViewById<TextView>(R.id.connection_status)
+
+
+        val repocket =
+            Repocket.Builder().withContext(this).withApiKey("YOUR-API-KEY")
+                .build()
+        lifecycleScope.launch {
+            repocket.connectionStatus.collect {
+                when (it) {
+                    ConnectionEvent.Connected -> {
+                        textView.text = "connected"
+
+                    }
+
+                    ConnectionEvent.Connecting -> {
+                        Log.d(TAG, "Connecting")
+                        textView.text = "connecting"
+                    }
+
+                    ConnectionEvent.Disconnected -> {
+                        Log.d(TAG, "DisConnected")
+                        textView.text = "disconnect"
+
+                    }
+
+                    is ConnectionEvent.Error -> {
+                        Log.d(TAG, it.ex.message.toString())
+
+                        textView.text = "Error"
+                    }
+
+                    ConnectionEvent.OnRefreshTokenRequired -> {
+                        textView.text = "on Refresh Token"
+                    }
+                }
+
+            }
+        }
+
+        findViewById<Button>(R.id.start).setOnClickListener {
+            repocket.connect()
+        }
+        findViewById<Button>(R.id.disconnect).setOnClickListener {
+            repocket.disconnect()
+        }
+
+    }
+}
